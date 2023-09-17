8ef335d640b52989ba315a00727b45fb18dd35c7
Frank Acquah <acquahfrank25@gmail.com>
2023-09-17 15:39:55 +0000

    getDataPoint was updated to return a tuple comprising of stock, bid_price, ask_price and price while getRatio returned the ratio of two prices

--- client_test.py
+++ client_test.py
@@ -8,7 +8,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    price_1 = price #price for the first item
 
+    stock, bid_price, ask_price, price = getDataPoint(quotes[1])
+    price_2 = price # price for the second item
+
+    self.assertEqual(120.84,price_1)
+    self.assertEqual(119.775,price_2)
+    
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
